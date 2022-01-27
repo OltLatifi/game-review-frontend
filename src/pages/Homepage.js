ba4af3f27493f5a8f9733269f296eb2439a57cc8
Olt Latifi <oltlatifi2003@gmail.com>
2022-01-27 13:56:39 +0100

    using queries for 'fetching' the graphql api

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import useFetch from '../hooks/useFetch';
+// import useFetch from '../hooks/useFetch';
 import {Link} from 'react-router-dom';
+import { useQuery, gql } from "@apollo/client";
+
+
+const REVIEWS = gql`
+  query GetReviews{
+    reviews{
+      data{
+        id,
+        attributes{
+          title,
+          body,
+          rating
+        }
+      }
+    }
+  }
+`
 
 function Homepage() {
-  const {data, error, loading} = useFetch("http://localhost:1337/api/reviews");
+  // const {data, error, loading} = useFetch("http://localhost:1337/api/reviews");
+  const {data, error, loading} = useQuery(REVIEWS);
   if(loading) return <p>loading</p>
   if(error) return <p>{error}...</p>
+  const deconstructedData = data.reviews.data
 
   return(
   <div>
-    {data.map(review =>{
+    {deconstructedData.map(review =>{
       return(
         <div key={review.id} className="review-card">
         <div className="rating">{review.attributes.rating}</div>
